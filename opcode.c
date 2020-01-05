7de1bd870eca26b10731ab008642b1941229ac65
pafuentess <pafuentess@unal.edu.co>
2020-01-05 13:34:02 +0000

    como yuli lo tenia

--- opcode.c
+++ opcode.c
@@ -49,7 +49,7 @@ void pall(stack_t **stack, unsigned int line_number)
 	tmp = *stack;
 	while (tmp)
 	{
-		printf("%i\n", tmp->n);
+		fprintf(stdout, "%i\n", tmp->n);
 		tmp = tmp->next;
 		if (tmp == *stack)
 			return;
