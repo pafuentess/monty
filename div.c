1e0c8a6a044040e623aa92d06d178c269dc68b67
pafuentess <pafuentess@unal.edu.co>
2020-01-05 20:25:50 +0000

    div

--- div.c
+++ div.c
@@ -6,8 +6,8 @@
 */
 void div_fun(stack_t **stack, unsigned int line_number)
 {
-	int len = 0, a = 0, b = 0;
-	stack_t *temp, *temp1, *temp2;
+	int len = 0, a = 0;
+	stack_t *temp;
 
 	temp = *stack;
 	while (temp)
@@ -20,17 +20,13 @@ void div_fun(stack_t **stack, unsigned int line_number)
 		fprintf(stderr, "L%d: can't div, stack too short\n", line_number);
 		exit(EXIT_FAILURE);
 	}
-	temp1 = *stack;
-	temp2 = temp1->next;
-	a = temp1->n;
-	b = temp2->n;
+	a = (*stack)->n;
+	pop(stack, line_number);
 	if (a == 0)
 	{
 		fprintf(stderr, "L%d: division by zero", line_number);
 		exit(EXIT_FAILURE);
 	}
-	temp2->n = b / a;
-	*stack = temp2;
-	pop(&temp1, line_number);
+	(*stack)->n = (*stack)->n / a;
 }
 
