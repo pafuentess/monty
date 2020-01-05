11a648c151a46dbc6f90734e888022b80d18c42c
pafuentess <pafuentess@unal.edu.co>
2020-01-05 18:54:48 +0000

    add message error

--- pop.c
+++ pop.c
@@ -10,7 +10,10 @@ void pop(stack_t **stack, unsigned int line_number)
 	(void) line_number;
 
 	if (stack == NULL || *stack == NULL)
+	{	
+		fprintf(stderr, "L%d: can't pop an empty stack\n", line_number);
 		exit(EXIT_FAILURE);
+	}
 
 	tmp = *stack;
 	*stack = tmp->next;
