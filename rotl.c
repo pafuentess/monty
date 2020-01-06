dc1152659fdb01c6df0cd9e05bb18fa679cb718a
pafuentess <pafuentess@unal.edu.co>
2020-01-06 03:44:59 +0000

    fix rotl

--- rotl.c
+++ rotl.c
@@ -8,19 +8,21 @@
 
 void rotl(stack_t **stack, unsigned int line_number)
 {
-	stack_t *tmp1 = *stack;
+	stack_t *tmp1 = *stack, *tmp2 = *stack, *tmp3 = NULL;
 	(void) line_number;
 
 	if (*stack == NULL)
 		return;
+	tmp3 = tmp1->next;
 	while (tmp1->next != NULL)
 	{
 		tmp1 = tmp1->next;
 	}
 	tmp1->next = *stack;
-	(*stack)->prev = tmp1;
-	tmp1 = (*stack)->next;
-	(*stack)->next = NULL;
-	*stack = tmp1;
+	tmp2->prev = tmp1;
+	tmp2->next = NULL;
+	*stack = tmp3;
 	(*stack)->prev = NULL;
+	
+	
 }
